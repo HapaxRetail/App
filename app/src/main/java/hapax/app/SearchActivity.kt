2b8f0fbc3737f42fd60744b16aeaab6422e56988
Jacob Evans <levansj01@gmail.com>
2021-07-17 17:11:51 +0100

    Cleanup

--- app/src/main/java/hapax/app/SearchActivity.kt
+++ app/src/main/java/hapax/app/SearchActivity.kt
@@ -22,21 +22,23 @@ class SearchActivity : AppCompatActivity() {
         }, 1000)
         super.onCreate(savedInstanceState)
 
-        val binding = SearchLayoutBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        SearchLayoutBinding.inflate(layoutInflater).apply {
+            setContentView(root)
 
-        binding.rvStores.adapter = adapter
-        binding.rvStores.layoutManager = LinearLayoutManager(this)
-        binding.searchView.setOnQueryTextListener(SimpleTextListener { search ->
-            storeList.enqueue(SimpleCallback { stores ->
-                val results =
-                    if(search.isEmpty()) emptyList()
-                    else stores.filter { name -> name.startsWith(search, true) }
+            rvStores.adapter = adapter
+            rvStores.layoutManager = LinearLayoutManager(this@SearchActivity)
+            searchView.setOnQueryTextListener(SimpleTextListener { search ->
+                storeList.enqueue(SimpleCallback { stores ->
+                    val results =
+                        if(search.isEmpty()) emptyList()
+                        else stores.filter { name -> name.startsWith(search, true) }
 
-                adapter.search( results )
-                binding.rvStores.setPadding(0, if (results.isEmpty()) 0 else 40, 0, 0)
+                    adapter.search( results )
+                    rvStores.setPadding(0, if (results.isEmpty()) 0 else 40, 0, 0)
+                })
+                true
             })
-            true
-        })
+        }
     }
+
 }
