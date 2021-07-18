8a68f9d63f76cc1cc04d4bd6ada97a665dc04f34
Jacob Evans <levansj01@gmail.com>
2021-07-18 15:37:35 +0100

    Implement navigation menu

---
+++ app/src/main/java/hapax/app/FragmentProfile.kt
@@ -0,0 +1,5 @@
+package hapax.app
+
+import androidx.fragment.app.Fragment
+
+class FragmentProfile : Fragment(R.layout.fragment_profile)
--- app/src/main/java/hapax/app/MainActivity.kt
+++ app/src/main/java/hapax/app/MainActivity.kt
@@ -1,13 +1,22 @@
 package hapax.app
 
+import android.app.Activity
 import android.os.Bundle
+import android.view.MenuItem
+import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.commit
+import com.google.android.material.navigation.NavigationView
 import hapax.app.databinding.ActivityMainBinding
 import java.util.*
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity() , NavigationView.OnNavigationItemSelectedListener {
+    lateinit var drawer: DrawerLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         Timer().schedule(object: TimerTask() {
             override fun run() {
@@ -17,6 +26,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         ActivityMainBinding.inflate(layoutInflater).apply {
+            drawer = drawerLayout
             setContentView(root)
             val toolbar = findViewById<Toolbar>(R.id.menuToolbar).apply { title = "" }
             setSupportActionBar(toolbar)
@@ -24,8 +34,28 @@ class MainActivity : AppCompatActivity() {
             val toggle = ActionBarDrawerToggle(this@MainActivity, drawerLayout, toolbar, R.string.open, R.string.close)
             toggle.isDrawerIndicatorEnabled = true
             toggle.isDrawerSlideAnimationEnabled = true
-            drawerLayout.addDrawerListener(toggle)
+            drawer.addDrawerListener(toggle)
             toggle.syncState()
+
+            navMenu.setNavigationItemSelectedListener(this@MainActivity)
         }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        val clazz = when(item.itemId) {
+            R.id.nav_findStore -> FragmentStores::class.java
+            R.id.nav_profile -> FragmentProfile::class.java
+            else -> return super.onOptionsItemSelected(item)
+        }
+        supportFragmentManager.commit {
+            replace(R.id.include_layout, clazz, null, null)
+            setReorderingAllowed(true)
+            addToBackStack(null)
+        }
+        drawer.closeDrawers()
+
+        val service = getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+        service.hideSoftInputFromWindow(window.decorView.rootView.windowToken, 0)
+        return true
     }
 }
